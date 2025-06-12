0e12957c69c0aa0f20f373e9c83833786d5f3e85
Nguyen27806 <106240064@student.vgu.edu.vn>
2025-06-12 09:46:29 +0700

    Update sunburst2.py

--- sunburst2.py
+++ sunburst2.py
@@ -12,33 +12,32 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Title
-st.title("Sunburst Chart with Customizable Rings")
+st.title("Sunburst Chart: Filter by Field and Salary Group")
 
-# Two dropdowns
-include_field = st.selectbox(
-    "Include Field of Study?",
-    options=["Yes", "No"],
-    index=0
+# Dropdown 1: Filter Field_of_Study
+selected_fields = st.multiselect(
+    "Select Field(s) of Study:",
+    options=df['Field_of_Study'].unique(),
+    default=df['Field_of_Study'].unique()
 )
 
-include_salary = st.selectbox(
-    "Include Salary Group?",
-    options=["Yes", "No"],
-    index=0
+# Dropdown 2: Filter Salary_Group
+selected_salaries = st.multiselect(
+    "Select Salary Group(s):",
+    options=df['Salary_Group'].unique(),
+    default=df['Salary_Group'].unique()
 )
 
-# Construct path based on choices
-path = ['Entrepreneurship']
-if include_field == "Yes":
-    path.append('Field_of_Study')
-if include_salary == "Yes":
-    path.append('Salary_Group')
+# Filter dataframe based on selection
+filtered_df = df[
+    df['Field_of_Study'].isin(selected_fields) &
+    df['Salary_Group'].isin(selected_salaries)
+]
 
 # Create sunburst chart
 fig = px.sunburst(
-    df,
-    path=path,
+    filtered_df,
+    path=['Entrepreneurship', 'Field_of_Study', 'Salary_Group'],
     values='Count'
 )
 
@@ -49,5 +48,4 @@ fig.update_traces(
     hovertemplate="<b>%{label}</b><br>Value: %{value}<br>"
 )
 
-# Show chart
 st.plotly_chart(fig, use_container_width=True)
